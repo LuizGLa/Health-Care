e97ea179f5ce25775d36df160f802d76026f3ae6
Luiz Gustavo <luizgustavolima200@gmail.com>
2023-11-07 05:35:58 -0300

    Adição de arquivo para teste de produção

---
+++ src/dataClient.js
@@ -0,0 +1,3 @@
+// Remova o _example do arquivo e adicione seu client_id
+export const CLIENT_ID = '587860084574-kjlho9on0qfm5fslbhtge6l62k9i0tjj.apps.googleusercontent.com' 
+export const scope = 'email profile openid https://www.googleapis.com/auth/fitness.activity.read https://www.googleapis.com/auth/fitness.body.read https://www.googleapis.com/auth/fitness.body.write https://www.googleapis.com/auth/fitness.activity.write https://www.googleapis.com/auth/fitness.location.read https://www.googleapis.com/auth/fitness.location.write https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/fitness.body.read https://www.googleapis.com/auth/fitness.body.write'
