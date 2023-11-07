c919057f6262491cb7f5b34e9da000b19ca78e68
Luiz Gustavo <luizgustavolima200@gmail.com>
2023-11-07 06:33:50 -0300

    Teste de pwa

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+
+
   transpileDependencies: [
 
   ],
 
   pluginOptions: {
+   
+  },
 
+  pwa: {
+    name: 'Health Care',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    workboxPluginMode: 'GenerateSW'
   }
 })
