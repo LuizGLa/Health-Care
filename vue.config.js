98982f907f9eca488ffda6f9a5129f77ee953417
Luiz Gustavo <luizgustavolima200@gmail.com>
2023-11-07 17:47:38 -0300

    Add pwa

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,19 @@
 // Inside vue.config.js
 module.exports = {
   // ...outras opções do vue-cli...
-  pwa: {
-    name: 'My App',
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
+  // pwa: {
+  //   name: 'Health Care',
+  //   themeColor: '#4DBA87',
+  //   msTileColor: '#000000',
+  //   appleMobileWebAppCapable: 'yes',
+  //   appleMobileWebAppStatusBarStyle: 'black',
 
-    // Configurar o plugin workbox
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc é obrigatório no modo InjectManifest.
-      swSrc: 'dev/sw.js', // Caminho para o seu arquivo de trabalhador de serviço
-      // ...outras opções do Workbox...
-    }
-  }
+  //   // Configurar o plugin workbox
+  //   workboxPluginMode: 'InjectManifest',
+  //   workboxOptions: {
+  //     // swSrc é obrigatório no modo InjectManifest.
+  //     swSrc: 'dev/sw.js', // Caminho para o seu arquivo de trabalhador de serviço
+  //     // ...outras opções do Workbox...
+  //   }
+  // }
 };
